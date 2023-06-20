574b9669e3a9e44cdd7401bef206c302ede2a1a5
ThiagoCosmeSilva <127049774+ThiagoCosmeSilva@users.noreply.github.com>
2023-06-20 13:39:44 -0300

    Add files via upload

---
+++ Cadastro/Funcionarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    internal class Funcionarios
+    {
+        public string Nome { get; set; }
+        public string Setor { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
